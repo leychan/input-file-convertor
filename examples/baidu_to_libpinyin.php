6220dd0cad9922a921ca1bf9dc4179bbbf141345
chenlei <leilei1512159@gmail.com>
2020-04-14 18:02:35 +0800

    optimization

--- examples/baidu_to_libpinyin.php
+++ examples/baidu_to_libpinyin.php
@@ -5,8 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 use leychan\Convert\Convertor as Convertor;
 use leychan\Convert\FileHandler as FileHandler;
 
-$src = '/home/chenlei/tmp/ch3.txt';
-$dst = '/home/chenlei/tmp/ch3_cvt.txt';
+$user = getUser();
+
+$src = "/home/{$user}/tmp/ch3.txt";
+$dst = "/home/{$user}/tmp/ch3_cvt.txt";
 
 FileHandler::setReadResource($src);
 FileHandler::setWriteResource($dst);
@@ -21,3 +23,6 @@ while ($glob->valid()) {
 }
 
 FileHandler::closeWriteFile();
+function getUser() {
+    return trim(shell_exec('echo $USER'));
+}
